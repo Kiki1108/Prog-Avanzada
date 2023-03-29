554376eb8f3fde4c07e932a97828cbb5dff1ec6b
Kiki.Crush <111796763+Kiki1108@users.noreply.github.com>
2023-03-29 15:21:48 -0300

    Create c1.py

---
+++ u1/clases/2003/c1.py
@@ -0,0 +1,28 @@
+
+class Persona():
+    def __init__ (self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def muestra_nombre(self):
+        nombre = "María"
+        print(self.nombre)
+
+    def __cumpleanos():
+        pass
+
+    def __tiene_familia():
+        pass
+    
+    def __va_universidad():
+        pass
+
+
+def main():
+    alejandro = Persona("Alejandro", "Feo")
+
+    alejandro.muestra_nombre()
+
+
+if __name__ == "__main__":
+    main()
